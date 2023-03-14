0a3f7f63f12a83f43c9739dff5f1f2aa59352bee
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-03-14 22:10:33 +0000

    Add classmap autoload

--- backend/script.php
+++ backend/script.php
@@ -2,18 +2,6 @@
 
 require 'vendor/autoload.php';
 
-require 'include/App.php';
-require 'include/Lists/Ip.php';
-require 'include/Lists/Network.php';
-require 'include/Lists/Date.php';
-require 'include/Lists/Jail.php';
-require 'include/Lists/Country.php';
-require 'include/Ip.php';
-require 'include/Lists.php';
-require 'include/Logs.php';
-require 'include/Lookup.php';
-require 'include/Report.php';
-
 Lookup::setCountryDB('db/GeoLite2-Country.mmdb');
 Lookup::setAsnDB('db/GeoLite2-ASN.mmdb');
 
