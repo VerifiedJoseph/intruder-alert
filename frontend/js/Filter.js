2a84cc603c889e9aebef04d29c97e2c4371b0e16
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-03-18 19:49:01 +0000

    frontend: remove old Filter code

--- frontend/js/Filter.js
+++ frontend/js/Filter.js
@@ -8,10 +8,6 @@ export class Filter
 	}
 
 	getData(typeList) {
-		const network = document.getElementById('network-filter').value
-		const country = document.getElementById('country-filter').value
-		const jail = document.getElementById('jail-filter').value
-	
 		if (typeList === 'recentBans') {
 			var data = this.#getRecentBans()
 	
@@ -19,32 +15,7 @@ export class Filter
 			var data = this.data[typeList].list;
 		}
 	
-		console.log(this.#settings.length)
-
 		var filtered = []
-		var _settings = this.#settings
-
-		if (_settings.length > 0 && (typeList === 'ip' || typeList === 'recentBans') ) {
-
-			/*for (let index = 0; index < _settings.length; index++) {
-				var filter = _settings[index];
-
-				if (filter.type === 'jail' && typeList !== 'recentBans') {
-					continue;
-				}
-				
-				var add = false;
-				data.forEach(item => {
-					if (item[filter.type].toString() === filter.value) {
-						console.log('Match' + filter.value);
-						console.log(filter)
-	
-						if (filter.action === 'include') {
-							add = true
-						}
-					}
-				})
-			}*/
 
 			data.forEach(item => {
 				var addStatus = [];
@@ -82,55 +53,6 @@ export class Filter
 		}
 
 		return data
-
-		/*var filtered = data.filter(function (item) {
-			if (typeList === 'ip' || typeList === 'recentBans') {
-
-				for (let index = 0; index < _settings.length; index++) {
-					var include = false;
-					var filter = _settings[index];
-
-					if (filter.type === 'jail' && typeList !== 'recentBans') {
-						continue;
-					}
-				
-					if (filter.action === 'include')
-						include = true
-
-					console.log(include)
-
-					if (item[filter.type] === filter.value) {
-						console.log(filter.value );
-
-						return include;
-					}
-				}
-
-				/*this.settings.forEach(filter => {
-					
-					if (filter.type === 'jail' && typeList !== 'recentBans') {
-
-					}
-
-				});*/
-
-				/*if (network !== 'all' && network != item.network) {
-					return false
-				}
-	
-				if (country !== 'all' && country != item.country) {
-					return false
-				}
-
-				if (jail !== 'all' && jail != item.jail) {
-					return false
-				}*/
-		//	}
-		
-		//	return true
-		//})
-	
-		return filtered;
 	}
 
 	save() {
@@ -180,10 +102,6 @@ export class Filter
 	
 			select.appendChild(option)
 		})
-	}
-
-	resetOption(name) {
-		document.getElementById(`${name}-filter`).value = 'all'
 	}
 
 	disableOption(name) {
