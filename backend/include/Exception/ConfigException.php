11acdafd24457c94fae3cf306a4c75ad9d5c1f00
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-03-19 18:39:14 +0000

    Create ConfigException.php

---
+++ backend/include/Exception/ConfigException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Exception;
+
+use Throwable;
+
+class ConfigException extends \Exception
+{
+    public function __construct(string $message, int $code = 0, Throwable $previous = null)
+    {
+        $message = '[Config error] ' . $message;
+
+        parent::__construct($message, $code, $previous);
+    }
+}
