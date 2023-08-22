ed45ebc8ac43c0a6dd7b4b082b355d6009d7bd86
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-08-22 15:02:06 +0100

    ConfigException: Use sprintf

--- backend/include/Exception/ConfigException.php
+++ backend/include/Exception/ConfigException.php
@@ -6,8 +6,6 @@ class ConfigException extends \Exception
 {
     public function __construct(string $message, int $code = 0, \Throwable $previous = null)
     {
-        $message = '[Config error] ' . $message;
-
-        parent::__construct($message, $code, $previous);
+        parent::__construct(sprintf('Config error: %s', $message), $code, $previous);
     }
 }
