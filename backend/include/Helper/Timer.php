40445192bdcfe9c2827d33fed039755929ac2aba
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-08-22 15:01:56 +0100

    Timer: Fix indentation

--- backend/include/Helper/Timer.php
+++ backend/include/Helper/Timer.php
@@ -2,21 +2,21 @@
 
 class Timer
 {
-	private float $start = 0;
-	private float $end = 0;
+    private float $start = 0;
+    private float $end = 0;
 
-	public function start(): void
-	{
-		$this->start = microtime(true);
-	}
+    public function start(): void
+    {
+        $this->start = microtime(true);
+    }
 
-	public function stop(): void
-	{
-		$this->end = microtime(true);
-	}
+    public function stop(): void
+    {
+        $this->end = microtime(true);
+    }
 
-	public function getTime(): float
-	{
-		return round($this->end - $this->start, 3);
-	}
+    public function getTime(): float
+    {
+        return round($this->end - $this->start, 3);
+    }
 }
