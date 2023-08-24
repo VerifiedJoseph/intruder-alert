27a7589141e9980b3924fcf65e4879713a4f122e
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-08-24 14:45:15 +0100

    Update Networks.php

--- backend/include/List/Networks.php
+++ backend/include/List/Networks.php
@@ -11,10 +11,7 @@ class Networks extends AbstractList
     ];
 
     /** {@inheritDoc} */
-    protected array $settings = [
-        'calculateMostBanned' => true,
-        'orderBy' => 'bans'
-    ];
+    protected ?string $mostBannedParam = 'number';
 
     /** {@inheritDoc} */
     public function addIp(array $ip): void
