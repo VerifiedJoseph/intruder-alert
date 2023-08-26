a916fb1def9321697223d88333d722fea1b5b638
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-08-26 22:03:37 +0100

    Create TableFilter.js

---
+++ static/js/class/Filter/TableFilter.js
@@ -0,0 +1,27 @@
+import { Filter } from './Filter.js'
+
+export class TableFilter extends Filter {
+  #supportedListTypes = ['address', 'recentBans', 'subnet']
+  labelDiv = 'applied-filters'
+
+  /**
+   * Get filtered data
+   * @param {string} typeList
+   * @returns
+   */
+  getData (listType) {
+    let data
+
+    if (listType === 'recentBans') {
+      data = this.getRecentBans()
+    } else {
+      data = this.data[listType].list
+    }
+
+    if (this.settings.length > 0 && this.#supportedListTypes.includes(listType) === true) {
+      return this._getData(data, listType)
+    }
+
+    return data
+  }
+}
