3d0c525736fcc284ba7be35904c580610fec879a
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-08-27 19:13:25 +0100

    Update ChartFilter.js

--- static/js/class/Filter/ChartFilter.js
+++ static/js/class/Filter/ChartFilter.js
@@ -43,8 +43,8 @@ export class ChartFilter extends Filter {
    * @param {string} chartType Chart type
    */
   #groupByHour (data, chartType) {
+    const groupKeys = []
     const groups = []
-    const banCounts = []
 
     let yesterday = spacetime.now()
     yesterday = yesterday.subtract('24', 'hours')
@@ -55,13 +55,26 @@ export class ChartFilter extends Filter {
       if (timestamp.isAfter(yesterday) === true) {
         const timestampFormat = timestamp.format('{year}-{iso-month}-{date-pad} {hour-24-pad}:00')
 
-        if (groups.includes(timestampFormat) === true) {
-          const key = groups.indexOf(timestampFormat)
+        if (groupKeys.includes(timestampFormat) === true) {
+          const key = groupKeys.indexOf(timestampFormat)
 
-          banCounts[key]++
+          const group = groups[key]
+          group.banCount++
+
+          if (group.addresses.includes(item.address) === false) {
+            group.ipCount++
+            group.addresses.push(item.address)
+          }
         } else {
-          groups.push(timestampFormat)
-          banCounts.push(1)
+          const group = {
+            date: timestampFormat,
+            banCount: 1,
+            ipCount: 1,
+            addresses: [item.address]
+          }
+
+          groupKeys.push(timestampFormat)
+          groups.push(group)
         }
       } else {
         break
@@ -69,8 +82,8 @@ export class ChartFilter extends Filter {
     }
 
     return {
-      labels: groups.reverse(),
-      data: banCounts.reverse(),
+      labels: groupKeys.reverse(),
+      datasets: this.#getDatasets(groups),
       type: chartType
     }
   }
@@ -81,8 +94,8 @@ export class ChartFilter extends Filter {
    * @param {string} chartType Chart type
    */
   #groupByDay (data, days, chartType) {
+    const groupKeys = []
     const groups = []
-    const banCounts = []
 
     let lastWeek = spacetime.now()
     lastWeek = lastWeek.subtract(`${days}`, 'days')
@@ -93,23 +106,62 @@ export class ChartFilter extends Filter {
       if (timestamp.isAfter(lastWeek) === true) {
         const timestampFormat = timestamp.format('{year}-{iso-month}-{date-pad}')
 
-        if (groups.includes(timestampFormat) === true) {
-          const key = groups.indexOf(timestampFormat)
+        if (groupKeys.includes(timestampFormat) === true) {
+          const key = groupKeys.indexOf(timestampFormat)
 
-          banCounts[key]++
+          const group = groups[key]
+          group.banCount++
+
+          if (group.addresses.includes(item.address) === false) {
+            group.ipCount++
+            group.addresses.push(item.address)
+          }
+
+          groups[key] = group
         } else {
-          groups.push(timestampFormat)
-          banCounts.push(1)
+          const group = {
+            date: timestampFormat,
+            banCount: 1,
+            ipCount: 1,
+            addresses: [item.address]
+          }
+
+          groupKeys.push(timestampFormat)
+          groups.push(group)
         }
       } else {
         break
       }
     }
 
+    console.log(groups)
+
     return {
-      labels: groups.reverse(),
-      data: banCounts.reverse(),
+      labels: groupKeys.reverse(),
+      datasets: this.#getDatasets(groups),
       type: chartType
     }
+  }
+
+  #getDatasets (groups) {
+    const banCounts = []
+    const ipCounts = []
+
+    groups.reverse().forEach(g => {
+      banCounts.push(g.banCount)
+      ipCounts.push(g.ipCount)
+    })
+
+    return [
+      {
+        fill: true,
+        label: 'IPs',
+        data: ipCounts
+      },
+      {
+        fill: true,
+        label: 'Bans',
+        data: banCounts
+      }]
   }
 }
