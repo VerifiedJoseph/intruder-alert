d69b2c68e60381f876e50272c85a03c5268cae48
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-08-30 20:04:11 +0100

    Update Helper.js

--- static/js/class/Helper.js
+++ static/js/class/Helper.js
@@ -1,53 +1,5 @@
 export class Helper {
   /**
-   * Create a data view button
-   * @param {string} viewType Data view type
-   * @param {string} filterType Filter type
-   * @param {string} filterValue Filter value
-   * @param {string} context Context the button is being used
-   * @returns HTMLButtonElement
-   */
-  static createViewButton (viewType, filterType, filterValue, context = 'table') {
-    const button = document.createElement('button')
-
-    button.innerText = (viewType === 'address') ? 'View IPs' : 'View Bans'
-    button.classList.add('view')
-    button.setAttribute('data-view-type', viewType)
-    button.setAttribute('data-filter-type', filterType)
-    button.setAttribute('data-filter-value', filterValue)
-    button.setAttribute('data-context', context)
-    return button
-  }
-
-  /**
-   * Create data filter button for a table cell
-   * @param {string} dataType Data type
-   * @param {string} dataValue Data value
-   * @param {string} text Span text
-   * @param {Filter} Filter Filter class instance
-   * @returns HTMLSpanElement
-   */
-  static createFilterButton (dataType, dataValue, text, filter) {
-    const span = document.createElement('span')
-
-    span.innerText = text
-    span.setAttribute('title', text)
-
-    if (filter.hasFilter(dataType, dataValue) === false) {
-      const button = document.createElement('button')
-
-      button.innerText = 'Filter'
-      button.classList.add('row-filter')
-      button.setAttribute('title', `Filter ${dataType} to ${text}`)
-      button.setAttribute('data-type', dataType)
-      button.setAttribute('data-value', dataValue)
-      span.append(button)
-    }
-
-    return span
-  }
-
-  /**
    * Create most banned buttons
    *
    * @param {object} data Data
@@ -60,6 +12,27 @@ export class Helper {
         Helper.createViewButton('recentBans', type, data[type].mostBanned, 'most-banned')
       )
     })
+  }
+
+  static orderData (data) {
+    if (document.getElementById('data-order-by').disabled === true) {
+      return data
+    }
+
+    let orderBy = document.getElementById('data-order-by').value
+    if (document.getElementById('data-order-by').value === 'ips') {
+      orderBy = 'ipCount'
+    }
+
+    data.sort(function (a, b) {
+      if (orderBy === 'date') {
+        return new Date(b.date) - new Date(a.date)
+      }
+
+      return b[orderBy] - a[orderBy]
+    })
+
+    return data
   }
 
   static getViewType () {
