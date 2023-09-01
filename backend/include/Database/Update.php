b49d299fc4658a1c2354ef9f0ddf83385cc529ec
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-09-01 12:43:32 +0000

    Database\update: Add `getDatabasePaths()` (#31)

--- backend/include/Database/Update.php
+++ backend/include/Database/Update.php
@@ -30,7 +30,7 @@ class Update
                 mkdir($path);
             }
     
-            if ($this->isUpdateDue() === true) {
+            if ($this->updateRequired() === true) {
                 Output::text('Updating Geoip2 databases', log: true);
 
                 $client = new \tronovav\GeoIP2Update\Client([
@@ -56,20 +56,17 @@ class Update
         }
     }
 
-    private function isUpdateDue(): bool
+    /**
+     * Check if a database update is required
+     */
+    private function updateRequired(): bool
     {
-        foreach ($this->editions as $edition) {
-            if ($edition == 'GeoLite2-ASN') {
-                $file = $this->config->getAsnDatabasePath();
-            } else {
-                $file = $this->config->getCountryDatabasePath();
-            }
-
-            if (File::exists($file) === false) {
+        foreach ($this->getDatabasePaths() as $path) {
+            if (File::exists($path) === false) {
                 return true;
             }
 
-            $fileModTime = (int) filemtime($file);
+            $fileModTime = (int) filemtime($path);
 
             if ($this->calculateTimeDiff($fileModTime) >= 86400) {
                 return true;
@@ -79,11 +76,30 @@ class Update
         return false;
     }
 
+    /**
+     * Calculate the difference between a file's last mod unix time and now
+     * 
+     * @param int $lastMod Last modified unix timestamp of a file
+     * @return int
+     */
     private function calculateTimeDiff(int $lastMod): int
     {
         $now = time();
         $diff = $now - $lastMod;
 
         return $diff;
+    }
+
+    /**
+     * Get GeoIP2 database paths
+     * 
+     * @return array<int, string>
+     */
+    private function getDatabasePaths(): array
+    {
+        return [
+            $this->config->getAsnDatabasePath(),
+            $this->config->getCountryDatabasePath()
+        ];
     }
 }
