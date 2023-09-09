5a8750bc7527dda46a5ffdd9cfdd0ec21917dd25
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-09-09 23:43:19 +0100

    Update ChartFilter.js

--- static/js/class/Filter/ChartFilter.js
+++ static/js/class/Filter/ChartFilter.js
@@ -106,24 +106,17 @@ export class ChartFilter extends Filter {
     const groupKeys = groupParts[0]
     const groups = groupParts[1]
 
-    const date = spacetime.now(this.iaData.getTimezone()).subtract(`${days - 1}`, 'days')
-
     for (const item of data) {
-      const timestamp = spacetime(item.timestamp, this.iaData.getTimezone())
+      const parts = item.timestamp.split(' ')
+      const key = groupKeys.indexOf(parts[0])
 
-      if (timestamp.isAfter(date) === true) {
-        const key = groupKeys.indexOf(timestamp.format(this.#dateDisplayFormat))
+      if (groups[key]) {
+        groups[key].banCount++
 
-        if (groups[key]) {
-          groups[key].banCount++
-
-          if (groups[key].addresses.includes(item.address) === false) {
-            groups[key].ipCount++
-            groups[key].addresses.push(item.address)
-          }
+        if (groups[key].addresses.includes(item.address) === false) {
+          groups[key].ipCount++
+          groups[key].addresses.push(item.address)
         }
-      } else {
-        break
       }
     }
 
