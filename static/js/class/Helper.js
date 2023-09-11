27ce451fc34754b924642ade85364af26ed5c519
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-09-11 22:50:18 +0100

    Update Helper.js

--- static/js/class/Helper.js
+++ static/js/class/Helper.js
@@ -51,4 +51,16 @@ export class Helper {
   static setChartType (value) {
     document.getElementById('chart-type').value = value
   }
+
+  static errorMessage (text, hideAfter = false) {
+    const error = document.getElementById('error')
+    error.classList.remove('hide')
+    error.innerText = text
+
+    if (hideAfter === true) {
+      setTimeout(() => {
+        error.classList.add('hide')
+      }, 5000)
+    }
+  }
 }
