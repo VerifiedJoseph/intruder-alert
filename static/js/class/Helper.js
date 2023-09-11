d018e540e9ac994b76c86607f610bbc2880ea010
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-09-11 22:11:00 +0000

    Helper(js): Move `orderData()` to index.js (#54)

--- static/js/class/Helper.js
+++ static/js/class/Helper.js
@@ -19,27 +19,6 @@ export class Helper {
     })
   }
 
-  static orderData (data) {
-    if (document.getElementById('data-order-by').disabled === true) {
-      return data
-    }
-
-    let orderBy = document.getElementById('data-order-by').value
-    if (document.getElementById('data-order-by').value === 'ips') {
-      orderBy = 'ipCount'
-    }
-
-    data.sort(function (a, b) {
-      if (orderBy === 'date') {
-        return new Date(b.date) - new Date(a.date)
-      }
-
-      return b[orderBy] - a[orderBy]
-    })
-
-    return data
-  }
-
   static getViewType () {
     return document.getElementById('table-type').value
   }
