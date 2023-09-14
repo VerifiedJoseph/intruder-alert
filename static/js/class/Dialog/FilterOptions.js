a193174ad5d084add6e63075a341bfee4b848c6d
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-09-14 12:06:02 +0100

    Update FilterOptions.js

--- static/js/class/Dialog/FilterOptions.js
+++ static/js/class/Dialog/FilterOptions.js
@@ -1,11 +1,29 @@
 import { Dialog } from './Dialog.js'
 
 export class FilterOptionsDialog extends Dialog {
-  dialogType = 'filter-add'
+  dialogType = 'filter-options'
 
   constructor (viewType) {
     super(viewType)
 
     this.setElement()
+  }
+
+  /**
+   * Setup dialog
+   * @param {Filter|ChartFilter} filter Filter class instance
+   */
+  setup (filter) {
+    console.log(filter.hasFilters())
+
+    if (filter.hasFilters() === true) {
+      document.getElementById('chart-filters-reverse').disabled = false
+      document.getElementById('chart-filters-remove').disabled = false
+      document.getElementById('chart-filter-a').disabled = false
+    } else {
+      document.getElementById('chart-filters-reverse').disabled = true
+      document.getElementById('chart-filters-remove').disabled = true
+      document.getElementById('chart-filter-a').disabled = true
+    }
   }
 }
