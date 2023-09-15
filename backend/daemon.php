b3da699567aabf278dbd75cbe71b80912a14a31b
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-09-15 14:56:50 +0000

    Add daemon script (#75)

---
+++ backend/daemon.php
@@ -0,0 +1,29 @@
+<?php
+
+use IntruderAlert\App;
+use IntruderAlert\Config;
+use IntruderAlert\Helper\Output;
+use IntruderAlert\Exception\ConfigException;
+use IntruderAlert\Exception\AppException;
+
+require 'vendor/autoload.php';
+
+Output::text('Starting intruder alert daemon...');
+
+while (true) {
+    try {
+        $config = new Config();
+        $config->setDir(__DIR__);
+        $config->check();
+
+        $app = new App($config);
+        $app->run();
+    } catch (AppException $err) {
+        Output::text($err->getMessage());
+    } catch (ConfigException $err) {
+        Output::text($err->getMessage());
+        exit(1);
+    }
+
+    sleep(600);
+}
