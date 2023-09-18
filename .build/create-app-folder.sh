e84ab1221c8c8083b6e535091e7e531fdc59f4ec
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-09-18 23:49:33 +0000

    workflows: Add zip archive creation to build (#114)

---
+++ .build/create-app-folder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Create app folder and copy needed files
+#
+mkdir -p ./app/
+
+cp -r ./backend/ ./app/backend/
+cp -r ./static/ ./app/static/
+cp ./index.html ./app/index.html
+cp ./data.html ./app/data.php
+
