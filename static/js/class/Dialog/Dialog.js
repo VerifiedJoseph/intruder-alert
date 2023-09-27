e7914c9d2b939b11a1723092d5a1ca19b534d17c
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-09-27 18:30:51 +0100

    Update Dialog.js

--- static/js/class/Dialog/Dialog.js
+++ static/js/class/Dialog/Dialog.js
@@ -1,3 +1,5 @@
+import { Helper } from '../Helper.js'
+
 export class Dialog {
   element
   dialogType
@@ -41,5 +43,34 @@ export class Dialog {
 
   setElement () {
     this.element = document.getElementById(`${this.viewType}-${this.dialogType}-dialog`)
+  }
+
+  /**
+   * Create header element for dialog
+   * @param {string} title Header title
+   * @param {boolean} showCloseBtn Show close button
+   * @param {string} btnDataValue Value of the button's `data-close-dialog` attribute
+   * @returns HTMLDivElement
+   */
+  createHeader (title, showCloseBtn = false, btnDataValue = '') {
+    const header = document.createElement('div')
+    header.setAttribute('id', 'header')
+
+    const span = document.createElement('span')
+    span.innerText = `${Helper.capitalizeFirstChar(title)}`
+
+    header.appendChild(span)
+
+    if (showCloseBtn === true) {
+      const closeBtn = document.createElement('button')
+      closeBtn.classList.add('dialog-close')
+      closeBtn.setAttribute('id', 'dialog-close')
+      closeBtn.setAttribute('data-close-dialog', btnDataValue)
+      closeBtn.innerText = 'Close'
+
+      header.appendChild(closeBtn)
+    }
+
+    return header
   }
 }
