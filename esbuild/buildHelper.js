d380dc3bd0ef2ae5bfb92881fa3fdc6deb24c30f
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-09-29 01:50:27 +0100

    Update buildHelper.js

--- esbuild/buildHelper.js
+++ esbuild/buildHelper.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const fs = require('fs')
+const fsp = require('fs/promises')
 const fsExtra = require('fs-extra')
 
 module.exports = class buildHelper {
@@ -7,33 +8,81 @@ module.exports = class buildHelper {
   #backendDestination = path.resolve('./dist/backend')
 
   /**
-   * Copy contents of backend folder to `dist/backend`
+   * Copy file
    */
-  copyBackendFolder () {
-    console.log(`Copying ${this.#backendSource} to ${this.#backendDestination}`)
+  async copyFile (filename) {
+    const source = path.resolve(`./frontend/${filename}`)
+    const destination = path.resolve(`./dist/${filename}`)
 
-    fsExtra.copy(this.#backendSource, this.#backendDestination, err => {
-      if (err) return console.error(err)
-      console.log(`Copied backend folder to ${this.#backendDestination}`)
-    })
-  }
+    console.log(`Copying ${source} to ${destination}`)
 
-  /**
-   * Remove backend folder from `dist`
-   */
-  removeBackendFolder () {
-    if (fs.existsSync(this.#backendDestination) === true) {
-      fs.rmSync(this.#backendDestination, { recursive: true })
+    try {
+      await fsExtra.copy(source, destination)
+      console.log(`Copied file to ${destination}`)
+    } catch (err) {
+      console.error(err)
     }
   }
 
   /**
-   * Remove backend symlink from `dist`
+   * Copy folder
+   * @param {string} source Source folder
+   * @param {string} destination Destination folder
    */
-  removeBackendSymlink () {
-    fs.readlink(this.#backendDestination, (err, target) => {
+  async copyFolder (source, destination) {
+    source = path.resolve(source)
+    destination = path.resolve(destination)
+
+    console.log(`Copying ${source} to ${destination}`)
+
+    try {
+      await fsExtra.copy(source, destination)
+      console.log(`Copied folder to ${destination}`)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  /**
+   * Remove folder
+   */
+  async removeFolder (folder) {
+    folder = path.resolve(folder)
+
+    try {
+      if (fs.existsSync(folder) === true) {
+        await fsp.rm(folder, { recursive: true })
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  /**
+   * Create symlink
+   */
+  async createSymlink (source, destination) {
+    source = path.resolve(source)
+    destination = path.resolve(destination)
+
+    if (fs.existsSync(destination) === false) {
+      fs.symlink(
+        source,
+        destination,
+        'dir', (err) => err && console.log(err)
+      )
+    }
+  }
+
+  /**
+   * Remove symlink
+   */
+  async removeSymlink (symlink) {
+    symlink = path.resolve(symlink)
+
+    fs.readlink(symlink, (err, target) => {
       if (target !== undefined) {
-        fs.unlink(this.#backendDestination, err => {
+        fs.unlink(symlink, err => {
           if (err) console.log(err)
         })
       } else if (err) {
