8a606166cd34ee8fec99081bb658557b4c2f710f
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-10-08 16:15:53 +0100

    Update Addresses.php

--- backend/include/List/Addresses.php
+++ backend/include/List/Addresses.php
@@ -15,7 +15,6 @@ class Addresses extends AbstractList
         $address = $ip['address'];
 
         if (array_key_exists($address, $this->data['list']) === false) {
-            $this->totalBans++;
             $this->data['list'][$address] = [
                 'address' => $ip['address'],
                 'version' => $ip['version'],
@@ -24,19 +23,13 @@ class Addresses extends AbstractList
                 'continent' =>  $ip['continent']['code'],
                 'network' => $ip['network']['number'],
                 'subnet' => $ip['network']['subnet'],
-                'events' => [[
-                    'timestamp' => $ip['timestamp'],
-                    'jail' => $ip['jail']
-                ]]
             ];
         } else {
-            $this->totalBans++;
             $this->data['list'][$address]['bans']++;
-            $this->data['list'][$address]['events'][] = [
-                'timestamp' => $ip['timestamp'],
-                'jail' => $ip['jail']
-            ];
         }
+
+        $this->totalBans++;
+        $this->data['list'][$address]['events'][$ip['jail']][] = $ip['timestamp'];
     }
 
     /**
@@ -45,29 +38,5 @@ class Addresses extends AbstractList
     public function getTotalBans(): int
     {
         return $this->totalBans;
-    }
-
-    /**
-     * Order by date
-     */
-    protected function orderByDate(): void
-    {
-        $list = $this->data['list'];
-
-        foreach ($list as $itemKey => $item) {
-            usort($this->data['list'][$itemKey]['events'], function ($a1, $a2) {
-                $v1 = strtotime($a1['timestamp']);
-                $v2 = strtotime($a2['timestamp']);
-                return $v2 - $v1;
-            });
-
-            $this->data['list'][$itemKey]['firstSeen'] = $this->data['list'][$itemKey]['events'][0]['timestamp'];
-        }
-
-        usort($this->data['list'], function ($a1, $a2) {
-            $v1 = strtotime($a1['firstSeen']);
-            $v2 = strtotime($a2['firstSeen']);
-            return $v2 - $v1;
-        });
     }
 }
