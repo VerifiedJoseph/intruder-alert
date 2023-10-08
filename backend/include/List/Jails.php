20134f955c504c19849e50b4cffc30aaf5f01d81
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-10-08 14:35:48 +0100

    Update Jails.php

--- backend/include/List/Jails.php
+++ backend/include/List/Jails.php
@@ -16,22 +16,22 @@ class Jails extends AbstractList
     /** {@inheritDoc} */
     public function addIp(array $ip): void
     {
-        $key = array_search($ip['jail'], array_column($this->data['list'], 'name'));
+        $jail = $ip['jail'];
 
-        if ($key === false) {
-            $this->data['list'][] = [
+        if (array_key_exists($jail, $this->data['list']) === false) {
+            $this->data['list'][$jail] = [
                 'name' => $ip['jail'],
                 'ipCount' => 1,
                 'bans' => 1
             ];
 
-            $this->ipList[][] = $ip['address'];
+            $this->ipList[$jail][] = $ip['address'];
         } else {
-            $this->data['list'][$key]['bans']++;
+            $this->data['list'][$jail]['bans']++;
 
-            if (in_array($ip['address'], $this->ipList[$key]) === false) {
-                $this->ipList[$key][] = $ip['address'];
-                $this->data['list'][$key]['ipCount']++;
+            if (in_array($ip['address'], $this->ipList[$jail]) === false) {
+                $this->ipList[$jail][] = $ip['address'];
+                $this->data['list'][$jail]['ipCount']++;
             }
         }
     }
