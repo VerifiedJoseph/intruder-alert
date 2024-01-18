1362a40ff44748d18a2762ba64efc29a9b1fe323
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-01-18 23:05:35 +0000

    Update Update.php

--- backend/include/Database/Update.php
+++ backend/include/Database/Update.php
@@ -6,6 +6,7 @@ use IntruderAlert\Config;
 use IntruderAlert\Helper\File;
 use IntruderAlert\Helper\Output;
 use IntruderAlert\Exception\AppException;
+use IntruderAlert\Exception\FetchException;
 use Exception;
 
 class Update
@@ -41,7 +42,8 @@ class Update
                         Output::text('Updating Geoip2 database: ' . $edition, log: true);
 
                         $archivePath = $path . '.tar.gz';
-                        $checksum = $this->downloadChecksum($edition);
+                        $checksumFile = $this->downloadChecksum($edition);
+                        $checksum = $this->extractChecksum($checksumFile);
 
                         $this->downloadDatabase($edition, $archivePath);
                         $this->checkIntegrity($checksum['hash'], $archivePath);
@@ -111,28 +113,33 @@ class Update
      * Download checksum for a database
      *
      * @param string $edition Database edition
-     * @return array<string, string> Database checksum and filename
+     * @return string
      *
-     * @throws Exception if regex failed to extract checksum details from download file
+     * @throws Exception if downloading checksum file failed
      */
-    private function downloadChecksum(string $edition): array
+    private function downloadChecksum(string $edition): string
     {
-        $url = $this->buildUrl(
-            $edition,
-            $this->config->getMaxMindLicenseKey(),
-            'tar.gz.sha256'
-        );
+        try {
+            Output::text('Downloading checksum...');
 
-        $data = $this->fetch($url);
+            $url = $this->buildUrl(
+                $edition,
+                $this->config->getMaxMindLicenseKey(),
+                'tar.gz.sha256'
+            );
 
-        if (preg_match($this->checksumRegex, $data, $matches) !== 1) {
-            throw new Exception('Checksum details extraction failed');
+            $fetch = new Fetch($this->config->getUseragent());
+            $data = $fetch->get($url);
+
+            return $data;
+        } catch (FetchException $err) {
+            Output::text($err->getMessage(), log: true);
+
+            throw new Exception(sprintf(
+                'Failed to download checksum for %s',
+                $edition
+            ));
         }
-
-        return [
-            'hash' => $matches[1],
-            'filename' => $matches[2]
-        ];
     }
 
     /**
@@ -143,15 +150,48 @@ class Update
      */
     private function downloadDatabase(string $edition, string $path): void
     {
-        $url = $this->buildUrl(
-            $edition,
-            $this->config->getMaxMindLicenseKey(),
-            'tar.gz'
-        );
+        try {
+            Output::text('Downloading database...');
 
-        $data = $this->fetch($url);
+            $url = $this->buildUrl(
+                $edition,
+                $this->config->getMaxMindLicenseKey(),
+                'tar.gz'
+            );
 
-        File::write($path, $data);
+            $fetch = new Fetch($this->config->getUseragent());
+            $data = $fetch->get($url);
+
+            File::write($path, $data);
+        } catch (FetchException $err) {
+            Output::text($err->getMessage(), log: true);
+
+            throw new Exception(sprintf(
+                'Failed to download database for %s',
+                $edition
+            ));
+        }
+    }
+
+    /**
+     * Extract checksum from downloaded file
+     *
+     * @param string $data Checksum file data
+     * @param string $edition Database edition
+     * @return array<string, string> Database checksum and filename
+     *
+     * @throws Exception if regex failed to extract checksum details from download file
+     */
+    private function extractChecksum(string $data): array
+    {
+        if (preg_match($this->checksumRegex, $data, $matches) !== 1) {
+            throw new Exception('Checksum extraction failed');
+        }
+
+        return [
+            'hash' => $matches[1],
+            'filename' => $matches[2]
+        ];
     }
 
     /**
