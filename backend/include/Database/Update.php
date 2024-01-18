1cda7e4eca4757445f078fd85aabe9b57a6fd88f
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-01-18 23:07:43 +0000

    Update Update.php

--- backend/include/Database/Update.php
+++ backend/include/Database/Update.php
@@ -258,44 +258,6 @@ class Update
     }
 
     /**
-     * Fetch URL
-     *
-     * @param string $url URL
-     * @return string
-     *
-     * @throws Exception if an CURL error occurred
-     * @throws Exception if request returned non-200 status code
-     */
-    private function fetch(string $url): string
-    {
-        $ch = curl_init();
-        curl_setopt_array($ch, [
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_USERAGENT => $this->config->getUseragent()
-        ]);
-
-        $response = curl_exec($ch);
-        $error = curl_error($ch);
-        $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-        curl_close($ch);
-
-        if ($error !== '') {
-            throw new Exception($error);
-        }
-
-        if ($statusCode !== 200) {
-            throw new Exception(sprintf(
-                'Request failed. Returned HTTP %s',
-                $statusCode
-            ));
-        }
-
-        return (string) $response;
-    }
-
-    /**
      * Build MaxMind download Url
      *
      * @param string $edition Database edition
