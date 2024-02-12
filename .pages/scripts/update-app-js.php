c96b8d18b0547d4539b9b8a7172e30d45fb2a3ca
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-02-12 18:40:43 +0000

    Update demo to support charts (#361)

--- .pages/scripts/update-app-js.php
+++ .pages/scripts/update-app-js.php
@@ -1,9 +1,9 @@
 <?php
 #
-# Update fetch file path in index.js to use data.json.
+# Update fetch file path in app.js to use data.json.
 #
-$jsonFilepath = './.pages/data.json';
-$jsFilepath = './frontend/js/app.js';
+$jsonFilepath = '../data.json';
+$jsFilepath = '../../frontend/js/app.js';
 
 $data = file_get_contents($jsFilepath);
 
