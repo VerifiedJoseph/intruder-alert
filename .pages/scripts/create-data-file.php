ca8491d5b7c05f013f670a8caff4d361a240c4d3
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-02-12 18:31:52 +0000

    Create create-data-file.php

---
+++ .pages/scripts/create-data-file.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+	Script for creating the data.json used in the demo.
+ */
+
+include('../../backend/vendor/autoload.php');
+
+use IntruderAlert\Ip;
+use IntruderAlert\Lists;
+use IntruderAlert\Report;
+
+$eventsFilepath = '../events.json';
+$dataFilepath = '../data.json';
+
+$settings = [
+	'features' => [
+		'charts' => true,
+		'updates' => false,
+		'daemonLog' => false
+	],
+	'timezone' => 'Europe/London',
+	'version' => ''
+];
+
+function createLists(string $path) {
+	$file = file_get_contents($path);
+	$events = json_decode($file, associative: true);
+
+	$lists = new Lists();
+	foreach ($events as $event) {
+		$parts = explode(' ', $event['timestamp']);
+	
+		$dateNow = date('Y-m-d') . ' ' . $parts[1];
+		$date = new DateTime($dateNow);
+	
+		if ($parts[0] !== '0') {
+			$date->modify(sprintf('-%s day',  $parts[0]));
+		}
+	
+		$timestamp = $date->format('Y-m-d H:i:s');
+	
+		// Is timestamp in the future
+		if (strtotime($timestamp) > time()) {
+			continue;
+		}
+	
+		$ip = new Ip($event['ip']);
+		$ip->setJail($event['jail']);
+		$ip->setTimestamp($date->format('Y-m-d H:i:s'));
+		$ip->setCountry($event['country']);
+		$ip->setContinent($event['continent']);
+		$ip->setNetwork($event['network']);
+	
+		$lists->addIp($ip->getDetails());
+	}
+
+	return $lists;
+}
+
+function createReport(Lists $lists, string $path) {
+	$report = new Report(
+		$lists->get(),
+		$lists->getCounts(),
+		$path
+	);
+
+	$report->generate();
+}
+
+function addSettings(array $settings, string $path) {
+	$file = file_get_contents($path);
+	$data = json_decode($file, associative: true);
+
+	$data['settings'] = $settings;
+
+	$file = json_encode($data);
+	file_put_contents($path, $file);
+}
+
+$lists = createLists($eventsFilepath);
+createReport($lists, $dataFilepath);
+addSettings($settings, $dataFilepath);
