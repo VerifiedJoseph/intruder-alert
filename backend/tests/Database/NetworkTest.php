f94bda94dc64feb839677df0127a1987a3f3186f
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-03-10 15:35:37 +0000

    Add tests for Database\Network

---
+++ backend/tests/Database/NetworkTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use IntruderAlert\Database\Network;
+
+class NetworkTest extends TestCase
+{
+    private static Network $database;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$database = new Network('./backend/tests/files/mmdb/GeoLite2-ASN-Test.mmdb');
+    }
+
+    /**
+     * Test `lookup()`
+     */
+    public function testLookup(): void
+    {
+        $expected = [
+            'name' => 'Google Inc.',
+            'number' => 15169,
+            'subnet' => '1.0.0.0/24'
+        ];
+
+        $this->assertEquals(
+            $expected,
+            self::$database->lookup('1.0.0.1')
+        );
+    }
+
+    /**
+     * Test `lookup()` with an Address not in the database
+     */
+    public function testLookupAddressNotFound(): void
+    {
+        $this->expectOutputRegex('/Address not found in GeoIP2 ASN database/');
+
+        $expected = [
+            'name' => 'Unknown',
+            'number' => 'Unknown',
+            'subnet' => 'Unknown'
+        ];
+
+        $this->assertEquals(
+            $expected,
+            self::$database->lookup('127.0.0.1')
+        );
+    }
+}
