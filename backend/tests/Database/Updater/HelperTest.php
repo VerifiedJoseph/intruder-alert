65a0415fd3e371ecc5283325f1efa0c396c635c0
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-03-12 20:02:17 +0000

    Create HelperTest.php

---
+++ backend/tests/Database/Updater/HelperTest.php
@@ -0,0 +1,44 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use MockFileSystem\MockFileSystem as mockfs;
+use IntruderAlert\Database\Updater\Helper;
+
+class HelperTest extends TestCase
+{
+    public function setup(): void
+    {
+        mockfs::create();
+    }
+
+    /**
+     * Test `checkIntegrity()`
+     */
+    public function testCheckIntegrity(): void
+    {
+        $this->expectNotToPerformAssertions();
+
+        $file = mockfs::getUrl('/test.file');
+        file_put_contents($file, uniqid());
+        $checksum = hash_file('sha256', $file);
+
+        $helper = new Helper();
+        $helper->checkIntegrity($checksum, $file);
+    }
+
+    /**
+     * Test `checkIntegrity()` with a hashes that do not match
+     */
+    public function testCheckIntegrityFailure(): void
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Integrity check failed');
+
+        $file = mockfs::getUrl('/test.file');
+        file_put_contents($file, uniqid());
+        $checksum = hash_file('sha1', $file);
+
+        $helper = new Helper();
+        $helper->checkIntegrity($checksum, $file);
+    }
+}
