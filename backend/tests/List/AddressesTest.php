7f377e45ce83b0a226fee4435674d3051d07477f
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-03-12 15:51:36 +0000

    Update AddressesTest.php

--- backend/tests/List/AddressesTest.php
+++ backend/tests/List/AddressesTest.php
@@ -5,20 +5,24 @@ use IntruderAlert\List\Addresses;
 
 class AddressesTest extends TestCase
 {
-    private static $data;
-    private static $expected;
     private static Addresses $listClass;
+
+    /** @var array<mixed> */
+    private static $data;
+
+    /** @var array<mixed> */
+    private static $expected;
 
     public static function setUpBeforeClass(): void
     {
         self::$listClass = new Addresses();
 
-        self::$data = json_decode(
+        self::$data = json_decode((string)
             file_get_contents('./backend/tests/files/list-data.json'),
             associative: true
         );
 
-        self::$expected = json_decode(
+        self::$expected = json_decode((string)
             file_get_contents('./backend/tests/files/lists/expected-address-list.json'),
             associative: true
         );
@@ -44,6 +48,16 @@ class AddressesTest extends TestCase
     public function testGet(): void
     {
         $this->assertEquals(self::$listClass->get(), self::$expected);
+    }
+
+    /**
+     * Test `getCount()`
+     *
+     * @depends testAddIp
+     */
+    public function testGetCount(): void
+    {
+        $this->assertEquals(6, self::$listClass->getCount());
     }
 
     /**
