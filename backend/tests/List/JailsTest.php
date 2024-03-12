66ddaed2982c83bf9a2b55c13afebfd01ad5d284
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-03-12 15:42:05 +0000

    Create JailsTest.php

---
+++ backend/tests/List/JailsTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use IntruderAlert\List\Jails;
+
+class JailsTest extends TestCase
+{
+    private static $data;
+    private static $expected;
+    private static Jails $listClass;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$listClass = new Jails();
+
+        self::$data = json_decode(
+            file_get_contents('./backend/tests/files/list-data.json'),
+            associative: true
+        );
+
+        self::$expected = json_decode(
+            file_get_contents('./backend/tests/files/lists/expected-jail-list.json'),
+            associative: true
+        );
+    }
+
+    /**
+     * Test `addIp()`
+     */
+    public function testAddIp(): void
+    {
+        $this->expectNotToPerformAssertions();
+
+        foreach (self::$data['events'] as $item) {
+            self::$listClass->addIp($item);
+        }
+    }
+
+    /**
+     * Test `get()`
+     * 
+     * @depends testAddIp
+     */
+    public function testGet(): void
+    {
+        $this->assertEquals(self::$listClass->get(), self::$expected);
+    }
+}
