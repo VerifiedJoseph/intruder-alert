4d74869eba4b91403c27d7c4301cf4eed8614554
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-03-12 22:27:40 +0000

    Update ExtractorTest.php

--- backend/tests/Database/Updater/ExtractorTest.php
+++ backend/tests/Database/Updater/ExtractorTest.php
@@ -33,4 +33,33 @@ class ExtractorTest extends TestCase
         $extractor = new Extractor(new Config());
         $extractor->checksum('GeoLite2-ANS_19700101.tar.gz');
     }
+
+    /**
+     * Test `archive()`
+     */
+    public function testArchive(): void
+    {
+        // Path of test folder
+        $folder = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'intruder-alert-tests';
+        // Path of test archive
+        $archivePath = $folder . DIRECTORY_SEPARATOR . 'GeoLite2-ASN_19700101.tar.gz';
+        // Path of the extracted archive folder
+        $extractedArchivePath = $folder . DIRECTORY_SEPARATOR . 'GeoLite2-ASN_19700101';
+        // Path of extracted database
+        $extractedDatabasePath = $folder . DIRECTORY_SEPARATOR . 'GeoLite2-ASN.mmdb';
+
+        // Copy archive to test folder
+        copy('backend/tests/files/tar/has-mmdb/GeoLite2-ASN_19700101.tar.gz', $archivePath);
+
+        $config = $this->createStub(Config::class);
+        $config->method('getGeoIpDatabaseFolder')->willReturn($folder);
+
+        $extractor = new Extractor($config);
+        $extractor->archive($archivePath, 'GeoLite2-ASN', $extractedDatabasePath);
+
+        $this->assertFileExists($extractedDatabasePath);
+        $this->assertFileDoesNotExist($extractedArchivePath);
+
+        unlink($extractedDatabasePath);
+    }
 }
