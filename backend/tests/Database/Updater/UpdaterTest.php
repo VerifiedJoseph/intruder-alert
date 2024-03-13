a1a6c788db622d8fe90c5caa2a7904692ddad3e5
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-03-13 16:01:26 +0000

    Create UpdaterTest.php

---
+++ backend/tests/Database/Updater/UpdaterTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use IntruderAlert\Config;
+use IntruderAlert\Fetch;
+use IntruderAlert\Database\Updater\Updater;
+
+class UpdaterTest extends TestCase
+{
+	public function testWithNoMindLicenseKey(): void
+	{
+		$this->expectNotToPerformAssertions();
+
+		$config = new Config();
+		$fetch = new Fetch($config->getUseragent());
+		$updater = new Updater($config, $fetch);
+		$updater->run();
+	}
+}
