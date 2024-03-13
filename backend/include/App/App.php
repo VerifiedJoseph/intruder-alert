602ff455046750a2182000a3b66caaa9c21d5cfa
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-03-13 19:25:53 +0000

    Update App.php

--- backend/include/App/App.php
+++ backend/include/App/App.php
@@ -3,12 +3,16 @@
 namespace IntruderAlert\App;
 
 use IntruderAlert\Config;
+use IntruderAlert\Logger;
 use IntruderAlert\Lists;
 
 abstract class App
 {
     /** @var Config $config */
     protected Config $config;
+
+    /** @var Logger $logger */
+    protected Logger $logger;
 
     /** @var Lists $lists */
     protected Lists $lists;
@@ -25,6 +29,7 @@ abstract class App
     public function __construct(Config $config)
     {
         $this->config = $config;
+        $this->logger = new Logger();
         $this->lists = new Lists();
     }
 }
