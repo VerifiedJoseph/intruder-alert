9472912cddef2335cf0ffd16ae34459f98a1511d
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-03-13 20:21:20 +0000

    Update LoggerTest.php

--- backend/tests/LoggerTest.php
+++ backend/tests/LoggerTest.php
@@ -2,31 +2,36 @@
 
 use PHPUnit\Framework\TestCase;
 use IntruderAlert\Logger;
+use IntruderAlert\Helper\Output;
 
 class LoggerTest extends TestCase
 {
+    public static function setUpBeforeClass(): void
+    {
+        Output::disableQuiet();
+    }
+
     /**
      * Test `addEntry()`
      */
     public function testAddEntry(): void
     {
-        $this->expectOutputString('Hello World');
+        $this->expectOutputRegex('/Hello World/');
 
         $logger = new Logger();
-
         $logger->addEntry('Hello World');
+
         $this->assertEquals(['Hello World'], $logger->getEntries());
     }
 
     /**
-     * Test `addEntry()` with display boolean as false
+     * Test `addEntry()` with display boolean false
      */
     public function testAddEntryDisplayBooleanFalse(): void
     {
         $this->expectOutputString('');
 
         $logger = new Logger();
-
         $logger->addEntry('Hello World', display: false);
         $this->assertEquals(['Hello World'], $logger->getEntries());
     }
