8ff6d2e0e606ab3a956363ce320ff6a84c3225a8
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-03-13 18:03:15 +0000

    Add ReportTest

---
+++ backend/tests/ReportTest.php
@@ -0,0 +1,62 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use MockFileSystem\MockFileSystem as mockfs;
+use IntruderAlert\Report;
+use IntruderAlert\Lists;
+
+class ReportTest extends TestCase
+{
+    private static Lists $lists;
+
+    public static function setUpBeforeClass(): void
+    {
+        mockfs::create();
+
+        $data = self::getJsonFile('./backend/tests/files/list-data.json');
+
+        self::$lists = new Lists();
+        foreach ($data['events'] as $item) {
+            self::$lists->addIp($item);
+        }
+    }
+
+    /**
+     * Test `generate()`
+     */
+    public function testGenerate(): void
+    {
+        $this->expectOutputRegex('/Created report JSON file/');
+
+        $report = new Report(
+            self::$lists->get(),
+            self::$lists->getCounts(),
+            mockfs::getUrl('/report.json'),
+        );
+
+        $report->generate();
+
+        $expected = self::getJsonFile('./backend/tests/files/expected-report.json');
+        $actual = self::getJsonFile(mockfs::getUrl('/report.json'));
+
+        $this->assertGreaterThan(0, strtotime($actual['updated']));
+        $this->assertMatchesRegularExpression(
+            '/Last run: ([\d]{4}-[\d]{2}-[\d]{2} [\d]{2}:[\d]{2}:[\d]{2})/',
+            $actual['log'][0]
+        );
+
+        $actual['updated'] = '1970-01-01 00:00:00';
+        $actual['log'] = [];
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    private static function getJsonFile(string $path): array
+    {
+        return json_decode(
+            (string)
+            file_get_contents($path),
+            associative: true
+        );
+    }
+}
