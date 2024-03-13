131c64a7a8408a5a44b1fb9dee3b397e65040159
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-03-13 16:03:27 +0000

    Update UpdaterTest.php

--- backend/tests/Database/Updater/UpdaterTest.php
+++ backend/tests/Database/Updater/UpdaterTest.php
@@ -7,13 +7,13 @@ use IntruderAlert\Database\Updater\Updater;
 
 class UpdaterTest extends TestCase
 {
-	public function testWithNoMindLicenseKey(): void
-	{
-		$this->expectNotToPerformAssertions();
+    public function testWithNoMindLicenseKey(): void
+    {
+        $this->expectNotToPerformAssertions();
 
-		$config = new Config();
-		$fetch = new Fetch($config->getUseragent());
-		$updater = new Updater($config, $fetch);
-		$updater->run();
-	}
+        $config = new Config();
+        $fetch = new Fetch($config->getUseragent());
+        $updater = new Updater($config, $fetch);
+        $updater->run();
+    }
 }
