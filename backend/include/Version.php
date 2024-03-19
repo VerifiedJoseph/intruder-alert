7c5d45ea11d53ced2ffb582aaed344ff7880284e
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-03-19 15:18:49 +0000

    Version 1.14.0 (#452)

--- backend/include/Version.php
+++ backend/include/Version.php
@@ -7,7 +7,7 @@ class Version
     /**
      * @var string $version Intruder Alert version
      */
-    private static string $version = '1.13.1';
+    private static string $version = '1.14.0';
 
     /**
      * Returns version number
