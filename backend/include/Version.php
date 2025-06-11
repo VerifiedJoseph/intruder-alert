f391d28cac9a53c889ba407415547547bed19877
Joseph <VerifiedJoseph@users.noreply.github.com>
2025-06-11 18:21:27 +0100

    Bump version

--- backend/include/Version.php
+++ backend/include/Version.php
@@ -9,7 +9,7 @@ class Version
     /**
      * @var string $version Intruder Alert version
      */
-    private static string $version = '1.22.0';
+    private static string $version = '1.22.1';
 
     /**
      * Returns version number
