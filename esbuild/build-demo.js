8dec3873e09deeaeb4c760d6bdc869cc3242c5ee
Joseph <VerifiedJoseph@users.noreply.github.com>
2025-08-11 12:44:42 +0100

    Remove fs-extra dependency (#885)
    Rewrites esbuild scripts to remove dependency on fs-extra.

--- esbuild/build-demo.js
+++ esbuild/build-demo.js
@@ -1,15 +1,12 @@
 const esbuild = require('esbuild')
 const Helper = require('./class/Helper.js')
-
 const helper = new Helper()
 
 async function setup () {
-  // Remove and recreate dist folder
   await helper.removeFolder('./dist')
   await helper.createFolder('./dist')
 
-  console.log('Copying files...')
-  helper.copy('./frontend/index.html', './dist/index.html')
+  await helper.copyFile('./frontend/index.html', './dist/index.html');
 }
 
 setup().then(() => {
@@ -28,4 +25,6 @@ setup().then(() => {
     minify: true,
     outdir: 'dist/static'
   })
+}).catch((error) => {
+  console.error(error);
 })
